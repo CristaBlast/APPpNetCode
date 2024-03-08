f342f99291d609a88de03f39f144f4ab6a0f4752
Cristiano <cristianoben10@gmail.com>
2024-03-08 13:17:57 +0000

    08032024

---
+++ ApI/02.TailwindCSS.base/base/02.TailwindCSS/tailwind.config.js
@@ -0,0 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        },
+    },
+  },
+  plugins: [],
+}
+
