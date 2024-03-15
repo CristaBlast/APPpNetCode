f58d3fabe99dc711d7df5c0e7700fa50be5850c1
Cristiano <cristianoben10@gmail.com>
2024-03-15 12:06:13 +0000

    15032024

--- ApI/02.TailwindCSS.base/base/02.TailwindCSS/tailwind.config.js
+++ ApI/02.TailwindCSS.base/base/02.TailwindCSS/tailwind.config.js
@@ -1,15 +1,15 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-        },
-    },
-  },
-  plugins: [],
+ content: ["./src/**/*.{html,js}"],
+ theme: {
+ extend: {
+ fontFamily: {
+ sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+ },
+ },
+ },
+ plugins: [forms],
 }
-
